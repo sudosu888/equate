2ce49541c93a4cbec606de3311a516d6928c7a79
sudosu888 <zebbieman123@gmail.com>
2020-03-19 18:08:23 -0600

    removed old comment

--- equate.c
+++ equate.c
@@ -1,21 +1,7 @@
+/* See LICENSE file for license and copyright details */
 #include<stdio.h>
 #include<stdlib.h>
 
-/*
- * I'm hoping to turn this code into the new badcalculator, under the new
- * name "equate" I hope for it to work on Linux system, MacOS, and BSD with
- * the aim of POSIX compliancy, though as a beginner programmer I may not be
- * too sure how to do that. I wish to put this software under BSD-Clause 3 licence
- * when working and complete, and to be distributed through source code on github.
- *
- * currently, the switch case to do the calculation does not work, and cannot figure
- * out why. Once the code is working I plan to put up on github to test across other
- * UNIX and UNIX-Like systems.. I should probably also change the integers into
- * floats or doubles when it comes to division.
- *
- * ((NOTE)) REMOVE THIS STATEMENT UPON DISTRIBUTION
- *
- */
 int main(int argc, char *argv[])
 {
 	if(argc >= 3)
